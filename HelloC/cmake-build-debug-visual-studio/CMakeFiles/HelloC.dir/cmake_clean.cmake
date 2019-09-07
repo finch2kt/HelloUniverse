d7dff42919a02fead988efb9a40d51b77f9f4e6f
finch2kt <kashaunt.finch@gmail.com>
2019-09-06 20:45:00 -0400

    Add most Hello world projects to Repo

---
+++ HelloC/cmake-build-debug-visual-studio/CMakeFiles/HelloC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloC.dir/main.c.obj"
+  "HelloC.pdb"
+  "HelloC.exe"
+  "HelloC.exe.manifest"
+  "HelloC.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HelloC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
